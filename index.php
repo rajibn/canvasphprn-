ee8dc066be4867b45d3d9de0f2a482fb8bd09f1b
Your Name <rajib.naskar@codaemonsoftwares.com>
2018-02-28 20:14:23 +0530

    Authentication Key update

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ if ($signedRequest == null || $consumer_secret == null) {
 $sep = strpos($signedRequest, '.');
 $encodedSig = substr($signedRequest, 0, $sep);
 $encodedEnv = substr($signedRequest, $sep + 1);
-$calcedSig = base64_encode(hash_hmac("sha256", $encodedEnv, $consumer_secret, true));	  
+$calcedSig = base64_encode(hash_hmac("sha256", $encodedEnv, $consumer_secret, true));     
 if ($calcedSig != $encodedSig) {
    echo "Error: Signed Request Failed.  Is the app in Canvas?";
 }
@@ -34,36 +34,36 @@ $sr = base64_decode($encodedEnv);
 <script type="text/javascript" src="/scripts/json2.js"></script>
 <script src='scripts/ICanHaz.js'></script>
 <script>
-        var url = "/services/data/v26.0/query?q=8454525895187521239,canvasphprn";
-		var sr = JSON.parse('<?=$sr?>');
-		
+        var url = "/services/data/v26.0/query?q=SELECT+ID,NAME+FROM+ACCOUNT";
+        var sr = JSON.parse('<?=$sr?>');
+        
         $(document).ready(function() {
-		console.debug(sr);
-		$('#user-name').html(sr.context.user.fullName);
-		
-		
-		//within a Canvas iFrame, AJAX calls proxy via the window messaging
+        console.debug(sr);
+        $('#user-name').html(sr.context.user.fullName);
+        
+        
+        //within a Canvas iFrame, AJAX calls proxy via the window messaging
         Sfdc.canvas.client.ajax(url,
-            {	client : sr.client,
+            {   client : sr.client,
                 method: 'GET',
                 contentType: "application/json",
                 success : function(data) {
-						console.debug('Got Data');
-						console.debug(data);
-						$('#accountTable').append(ich.accounts(data.payload));
+                        console.debug('Got Data');
+                        console.debug(data);
+                        $('#accountTable').append(ich.accounts(data.payload));
                 }
             }); 
-		});
+        });
 </script>
 
 </script>
 Hello, <span id='user-name'></span>
 <table id="accountTable">
-	<tr><td><strong>Recent Accounts</strong></td></tr>
+    <tr><td><strong>Recent Accounts</strong></td></tr>
     <script type="text/html" id="accounts">
-			{{#records}}
+            {{#records}}
             <tr><td>{{Name}}</td></tr> 
-   			{{/records}}
+            {{/records}}
     </script>
 </table>
      
